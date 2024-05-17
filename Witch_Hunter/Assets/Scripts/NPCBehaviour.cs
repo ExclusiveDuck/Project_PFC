85a89e3b4ef8af81455948607c6290ea1dd906eb
ExclusiveDuck <ryan-willemsen@hotmail.com>
2024-05-17 20:50:37 +1000

    muz ai start

---
+++ Witch_Hunter/Assets/Scripts/NPCBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AIState { Patrol, Chase, Search }
+public class NPCBehaviour : MonoBehaviour
+{
+    public AIState myState = AIState.Patrol;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+   
+}
